8d8d71d67ecaa0f38e171ff3ff9b854ceed27658
Joellate <j.teta@alustudent.com>
2024-10-27 22:55:38 -0700

    task

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -5,4 +5,35 @@
 class Rectangle:
 
     """class Rectangle"""
-    pass
+    def __init__(self, width=0, height=0):
+        """initialize width and height"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """"retrieve width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ sets width"""
+        self.__width = value
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
+    @property
+    def height(self):
+        """define height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """assign height"""
+        self.__height = value
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
