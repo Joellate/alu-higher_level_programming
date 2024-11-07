b58d1291f27787322df12e8bb805fa9a1730fa1c
Joellate <j.teta@alustudent.com>
2024-11-07 02:45:42 -0800

    task

--- python-inheritance/4-main.py
+++ python-inheritance/4-main.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-inherits_from = _import_('4-inherits_from').inherits_from
+inherits_from = __import__('4-inherits_from').inherits_from
 
 a = True
 if inherits_from(a, int):
-    print("{} inherited from class {}".format(a, int._name_))
-
+    print("{} inherited from class {}".format(a, int.__name__))
 if inherits_from(a, bool):
-    print("{} inherited from class {}".format(a, bool._name_))
-
+    print("{} inherited from class {}".format(a, bool.__name__))
 if inherits_from(a, object):
-    print("{} inherited from class {}".format(a, object._name_))
+    print("{} inherited from class {}".format(a, object.__name__))
