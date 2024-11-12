0be2d8753435a7447750b791cdf8a0503442c6c2
Joellate <j.teta@alustudent.com>
2024-11-12 12:58:36 -0800

    task

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 def read_file(filename=""):
-    """Reads a UTF8 text file and prints its content to stdout."""
-    with open(filename, "r", encoding="utf-8") as file:
-        print(file.read(), end="")
+    """
+    Reads a UTF-8 encoded text file and prints its content to stdout.
+
+    Parameters:
+    filename (str): The path to the file to be read.
+    """
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read(), end="")
