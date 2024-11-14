91585dca2c7c1a65879d076ec68cbea5881c119d
Joellate <j.teta@alustudent.com>
2024-11-14 02:32:54 -0800

    task

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,4 +1,9 @@
+python3 1-write_file.py
 def write_file(filename="", text=""):
-    """Writes a string to a UTF8 text file and returns the number of characters written."""
-    with open(filename, "w", encoding="utf-8") as file:
+    """Writes a string to a text file and returns the number of characters written."""
+    with open(filename, 'w', encoding='utf-8') as file:
         return file.write(text)
+
+if __name__ == "__main__":
+    nb_characters = write_file("my_first_file.txt", "This School is so cool!\n")
+    print(nb_characters)
