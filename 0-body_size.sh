0c24774d61b15e2b607861776e2fbd1b85553cf4
root <j.teta@alustudent.com>
2024-11-26 12:30:09 +0000

    task

--- 0-body_size.sh
+++ 0-body_size.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-Check if a URL is passed as an argument
-if [ -z "$1" ]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-
-# Send a request using curl and get the response body size
-curl -s "$1" | wc -c
+#  script that take URL and   display size of the response
+curl -s $1 | wc -m
