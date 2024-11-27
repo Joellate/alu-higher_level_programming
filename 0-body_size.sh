9d3b051a4c68b752750e3b509039789a6cd21c92
root <j.teta@alustudent.com>
2024-11-27 09:20:14 +0000

    task

--- 0-body_size.sh
+++ 0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#  script that take URL and   display size of the response
-curl -s $1 | wc -m
+# Sends a request to the given URL and displays the size of the response body in bytes
+curl -s "$1" -o /dev/null -w "%{size_download}\n
